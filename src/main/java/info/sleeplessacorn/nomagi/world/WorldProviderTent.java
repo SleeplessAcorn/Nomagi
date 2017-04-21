17308a3224ef0e21706bbcb4f53bcab6354ec54a
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-04-20 23:48:35 -0700

    No more memory leak

--- src/main/java/info/sleeplessacorn/nomagi/world/WorldProviderTent.java
+++ src/main/java/info/sleeplessacorn/nomagi/world/WorldProviderTent.java
@@ -4,24 +4,14 @@ import info.sleeplessacorn.nomagi.core.ModObjects;
 import net.minecraft.init.Biomes;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
-import net.minecraft.world.biome.BiomeProvider;
 import net.minecraft.world.biome.BiomeProviderSingle;
 import net.minecraft.world.chunk.IChunkGenerator;
 
 public class WorldProviderTent extends WorldProvider {
 
-    public WorldProviderTent() {
-
-    }
-
     @Override
-    public long getWorldTime() {
-        return 6000;
-    }
-
-    @Override
-    public BiomeProvider getBiomeProvider() {
-        return new BiomeProviderSingle(Biomes.VOID);
+    protected void createBiomeProvider() {
+        biomeProvider = new BiomeProviderSingle(Biomes.VOID);
     }
 
     @Override
