5be0545f269ecd2765cca86117e2ad40ec2711f9
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-04-20 23:48:48 -0700

    No more model errors

--- src/main/java/info/sleeplessacorn/nomagi/item/ItemBlockTent.java
+++ src/main/java/info/sleeplessacorn/nomagi/item/ItemBlockTent.java
@@ -3,21 +3,12 @@ package info.sleeplessacorn.nomagi.item;
 import info.sleeplessacorn.nomagi.Nomagi;
 import info.sleeplessacorn.nomagi.core.ModObjects;
 import net.minecraft.item.ItemBlock;
-import tehnut.lib.mc.model.IModeled;
 
-import java.util.List;
-
-public class ItemBlockTent extends ItemBlock implements IModeled {
+public class ItemBlockTent extends ItemBlock {
 
     public ItemBlockTent() {
         super(ModObjects.TENT);
         setCreativeTab(Nomagi.TAB_NOMAGI);
         setUnlocalizedName(Nomagi.MODID + ".tent");
     }
-
-    @Override
-    public void getVariants(List<String> variants) {
-        variants.add("inventory");
-    }
-
 }
--- src/main/java/info/sleeplessacorn/nomagi/item/ItemNomagiDoor.java
+++ src/main/java/info/sleeplessacorn/nomagi/item/ItemNomagiDoor.java
@@ -14,12 +14,9 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import tehnut.lib.mc.model.IModeled;
-
-import java.util.List;
 
 // TODO - Move to LendingLibrary
-public class ItemNomagiDoor extends ItemBlock implements IModeled {
+public class ItemNomagiDoor extends ItemBlock {
 
     public ItemNomagiDoor(Block block) {
         super(block);
@@ -86,10 +83,5 @@ public class ItemNomagiDoor extends ItemBlock implements IModeled {
         world.setBlockState(pos.up(), placeSate.withProperty(BlockDoor.HALF, BlockDoor.EnumDoorHalf.UPPER), 2);
         world.notifyNeighborsOfStateChange(pos, door, false);
         world.notifyNeighborsOfStateChange(pos.up(), door, false);
-    }
-
-    @Override
-    public void getVariants(List<String> variants) {
-        variants.add("inventory");
     }
 }
