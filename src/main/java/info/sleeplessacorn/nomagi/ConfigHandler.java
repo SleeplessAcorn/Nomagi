db8e3e2d2b2fb4a80f5f920d83be993b621c4ba1
Nicholas Ignoffo <speedynutty68@gmail.com>
2017-09-30 01:32:49 -0700

    Fixed config entry names

--- src/main/java/info/sleeplessacorn/nomagi/ConfigHandler.java
+++ src/main/java/info/sleeplessacorn/nomagi/ConfigHandler.java
@@ -6,18 +6,15 @@ import net.minecraftforge.common.config.Config;
 @Config.LangKey("config.nomagi")
 public final class ConfigHandler {
 
-    @Config.Name("Tent Dimension ID")
     @Config.Comment({ "The ID that should be used for the tent dimension [default: 18]" })
     @Config.LangKey("config.nomagi.tentDimensionId")
     public static int tentDimensionId = 18;
 
-    @Config.Name("Tent Spacing")
     @Config.Comment({ "The number of empty chunks to generate between each generated tent [default: 32]" })
     @Config.RangeInt(min = 16, max = 128)
     @Config.LangKey("config.nomagi.tentSpacing")
     public static int tentSpacing = 32;
 
-    @Config.Name("Tent Radius")
     @Config.Comment({ "The maximum number of rooms a tent can create in any given direction from the core tent [default: 2]", "A value of 0 means no additional rooms will be permitted outside the core tent", "Ensure that the tent spacing is modified to account for the value set here" })
     @Config.RangeInt(min = 0, max = 16)
     @Config.LangKey("config.nomagi.tentRadius")
